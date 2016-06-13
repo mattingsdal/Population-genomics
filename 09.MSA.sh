b7e2e2620884adfb4ca48a603011a14233b246d3
Morten Mattingsdal <morten.mattingsdal@gmail.com>
2016-06-13 13:34:38 +0200

    Update 09.MSA.sh

--- 09.MSA.sh
+++ 09.MSA.sh
@@ -1,8 +1,19 @@
 # generate multiple sequence alignmnent using variants called by freebayes
-
+# vcffile must be zipped
 # generate consensus sequences for all individuals using bcftools from my.vcf
 
-    for D in *bam
-      do
-      bcftools consensus -f ~/genome/symphodus_melops.fasta -S $D freebayes.vcf z > ${D}}.fasta
-    done
+vcf=freebayes.SNPs.filtered.final.recode.vcf
+
+bgzip -c $vcf > ${vcf}.gz
+tabix -p vcf ${vcf}.gz
+
+mkdir fasta
+
+ls *BWA.rgfix.dedup.bam | cut -f 1 -d . >samples
+
+    while read p; do
+         bcftools consensus -f ~/genome/symphodus_melops.fasta -s $p $vcf.gz > fasta/${p}.fasta
+         done < samples
+         
+cd fasta
+
