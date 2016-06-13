d9a3364410f04601aa5cfab70974e65b22fd54b6
Morten Mattingsdal <morten.mattingsdal@gmail.com>
2016-06-13 13:11:18 +0200

    Update 09.MSA.sh

--- 09.MSA.sh
+++ 09.MSA.sh
@@ -1,1 +1,8 @@
 # generate multiple sequence alignmnent using variants called by freebayes
+
+# generate consensus sequences for all individuals using bcftools from my.vcf
+
+    for D in *bam
+      do
+      bcftools consensus -f ~/genome/symphodus_melops.fasta -S $D freebayes.vcf z > ${D}}.fasta
+    done
