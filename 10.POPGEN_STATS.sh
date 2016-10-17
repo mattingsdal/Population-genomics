5147be990f209fab7bb60eddae97a6e5f21e7e14
Morten Mattingsdal <morten.mattingsdal@gmail.com>
2016-10-17 15:41:12 +0200

    Update 10.POPGEN_STATS.sh

--- 10.POPGEN_STATS.sh
+++ 10.POPGEN_STATS.sh
@@ -1,16 +1,17 @@
 # calculate allele frequencies for each population
+$vcf=freebayes.SNPs.filtered.final.4.recode.vcf
 
 mkdir popgen
 
 ### FREQUENCY
-vcftools --vcf freebayes.SNPs.filtered.final.recode.vcf --keep ../pop/AR --freq2 --out popgen/AR 
-vcftools --vcf freebayes.SNPs.filtered.final.recode.vcf --keep ../pop/EG --freq2 --out popgen/EG 
-vcftools --vcf freebayes.SNPs.filtered.final.recode.vcf --keep ../pop/TV --freq2 --out popgen/TV
-vcftools --vcf freebayes.SNPs.filtered.final.recode.vcf --keep ../pop/ST --freq2 --out popgen/ST 
-vcftools --vcf freebayes.SNPs.filtered.final.recode.vcf --keep ../pop/NH --freq2 --out popgen/NH
-vcftools --vcf freebayes.SNPs.filtered.final.recode.vcf --keep ../pop/SM --freq2 --out popgen/SM 
-vcftools --vcf freebayes.SNPs.filtered.final.recode.vcf --keep ../pop/ARD --freq2 --out popgen/ARD 
-vcftools --vcf freebayes.SNPs.filtered.final.recode.vcf --keep ../pop/GF --freq2 --out popgen/GF 
+vcftools --vcf $vcf --keep ../pop/AR --freq2 --out popgen/AR 
+vcftools --vcf $vcf  --keep ../pop/EG --freq2 --out popgen/EG 
+vcftools --vcf $vcf  --keep ../pop/TV --freq2 --out popgen/TV
+vcftools --vcf $vcf  --keep ../pop/ST --freq2 --out popgen/ST 
+vcftools --vcf $vcf  --keep ../pop/NH --freq2 --out popgen/NH
+vcftools --vcf $vcf  --keep ../pop/SM --freq2 --out popgen/SM 
+vcftools --vcf $vcf  --keep ../pop/ARD --freq2 --out popgen/ARD 
+vcftools --vcf $vcf  --keep ../pop/GF --freq2 --out popgen/GF 
 
 
 ### TAJIMA'S D
@@ -23,14 +24,14 @@ vcftools --vcf freebayes.SNPs.filtered.final.recode.vcf --keep ../pop/SM --Tajim
 vcftools --vcf freebayes.SNPs.filtered.final.recode.vcf --keep ../pop/ARD --TajimaD 10000 --out popgen/ARD 
 
 # HHETEROZYGOSITY
-vcftools --vcf freebayes.SNPs.filtered.final.recode.vcf --keep ../pop/AR --het --out popgen/AR 
-vcftools --vcf freebayes.SNPs.filtered.final.recode.vcf --keep ../pop/EG --het --out popgen/EG 
-vcftools --vcf freebayes.SNPs.filtered.final.recode.vcf --keep ../pop/TV --het --out popgen/TV
-vcftools --vcf freebayes.SNPs.filtered.final.recode.vcf --keep ../pop/ST --het --out popgen/ST 
-vcftools --vcf freebayes.SNPs.filtered.final.recode.vcf --keep ../pop/NH --het --out popgen/NH
-vcftools --vcf freebayes.SNPs.filtered.final.recode.vcf --keep ../pop/SM --het --out popgen/SM 
-vcftools --vcf freebayes.SNPs.filtered.final.recode.vcf --keep ../pop/ARD --het --out popgen/ARD
-vcftools --vcf freebayes.SNPs.filtered.final.recode.vcf --keep ../pop/GF --het --out popgen/GF
+vcftools --vcf $vcf  --keep ../pop/AR --het --out popgen/AR 
+vcftools --vcf $vcf  --keep ../pop/EG --het --out popgen/EG 
+vcftools --vcf $vcf  --keep ../pop/TV --het --out popgen/TV
+vcftools --vcf $vcf  --keep ../pop/ST --het --out popgen/ST 
+vcftools --vcf $vcf  --keep ../pop/NH --het --out popgen/NH
+vcftools --vcf $vcf  --keep ../pop/SM --het --out popgen/SM 
+vcftools --vcf $vcf  --keep ../pop/ARD --het --out popgen/ARD
+vcftools --vcf $vcf  --keep ../pop/GF --het --out popgen/GF
 
 
 # HARDY-WINEBERG
