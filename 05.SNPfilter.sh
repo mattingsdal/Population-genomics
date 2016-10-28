3d970fbf418693219d1e37c521c0009aea920d16
Morten Mattingsdal <morten.mattingsdal@gmail.com>
2016-10-28 12:29:42 +0200

    Update 05.SNPfilter.sh

--- 05.SNPfilter.sh
+++ 05.SNPfilter.sh
@@ -65,18 +65,23 @@ plink --bfile plink_west_imputed --freq --out plink_west_imputed --allow-extra-c
 plink --bfile plink_south_imputed --bmerge plink_west_imputed.bed plink_west_imputed.bim plink_west_imputed.fam --allow-extra-chr --make-bed --out test --allow-extra-chr 
 plink --bfile test --bmerge plink_ard_imputed.bed plink_ard_imputed.bim plink_ard_imputed.fam --make-bed --out plink_beagle_imputed --allow-extra-chr
 
-# now remove SNPs with MAF < 5%
-
-# remove LD
+# remove rare SNPs
 plink --bfile plink_south_imputed --allow-extra-chr --maf 0.05 --make-bed --out plink_south_imputed_maf
 plink --bfile plink_west_imputed --allow-extra-chr --maf 0.05  --make-bed --out plink_west_imputed_maf
 plink --bfile plink_ard_imputed --allow-extra-chr --maf 0.07 --make-bed --out plink_ard_imputed_maf
 
 # merge dataset
-plink --bfile plink_south_imputed_maf --bmerge plink_west_imputed.bim plink_west_imputed.bed plink_west_imputed.fam --make-bed --out tmp --allow-extra-chr
-plink --bfile tmp --bmerge plink_ard_imputed_maf.bed plink_ard_imputed_maf.bim plink_ard_imputed_maf.fam --make-bed --out plink_impured_maf
+plink --bfile plink_south_imputed_maf --bmerge plink_west_imputed.bed plink_west_imputed.bim plink_west_imputed.fam --make-bed --out tmp --allow-extra-chr
+plink --bfile tmp --bmerge plink_ard_imputed_maf.bed plink_ard_imputed_maf.bim plink_ard_imputed_maf.fam --make-bed --out tmp2 --allow-extra-chr
+plink --bfile tmp2 --geno 0 --make-bed --out plink_imputed_maf
 
+# prune LD
+plink --bfile plink_imputed_maf --allow-extra-chr --indep-pairwise 20kb 20 0.5 --out plink_imputed_maf
+plink --bfile plink_imputed_maf --extract plink_imputed_maf.prune.in --make-bed --out plink_imputed_maf_noLD --allow-extra-chr
 
+# construct final dataset used for PCA, DAPC etc.
+plink --bfile plink_imputed_maf_noLD --allow-extra-chr --recodeA --out SNP.QC.population
+plink --bfile plink_imputed_maf_noLD --allow-extra-chr --make-bed --out SNP.QC.population
 
 
 #### VCF file with SNP names is now  "plink.bed" files
