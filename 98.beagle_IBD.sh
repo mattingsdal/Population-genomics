4ea970d3ee3d50b7fc5440abf029533eee4481f3
Morten Mattingsdal <morten.mattingsdal@gmail.com>
2018-02-27 09:41:14 +0100

    Update 98.beagle_IBD.sh

--- 98.beagle_IBD.sh
+++ 98.beagle_IBD.sh
@@ -1,1 +1,5 @@
 # http://faculty.washington.edu/sguy/beagle/ibd_and_hbd/ibd_and_hbd.html
+
+
+
+java -jar ../software/beagle.08Jun17.d8b.jar nthreads=10 ibd=true gt=freebayes.ALL.VAR.Q40.DP4_30_max_miss_5.recode.edit.annotated.names.SNPs.AA.mac2.vcf out=beagle ibdcm=0.001 ne=10000 ibdtrim=10
